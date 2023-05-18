50e616ebc61237ddc66096cda1a6d977f6c48ddc
IlsafCreator <ilsaf.khasanov@mail.ru>
2023-05-18 20:47:42 +0300

    make lw7

--- lw7/cmd/blog/handlers.go
+++ lw7/cmd/blog/handlers.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"html/template"
 	"log"
 	"net/http"
+	"strconv"
 
+	"github.com/gorilla/mux"
 	"github.com/jmoiron/sqlx"
 )
 
@@ -14,39 +17,55 @@ type indexPage struct {
 	MostRecent    []recentPostData
 }
 
-type postPage struct {
-	Title    string
-	Subtitle string
+type postData struct {
+	Title     string `db:"title"`
+	Subtitle  string `db:"subtitle"`
+	PostImage string `db:"image_url"`
+	Content   string `db:"content"`
 }
 
 type featuredPostData struct {
+	ID          string `db:"post_id"`
 	Title       string `db:"title"`
 	Subtitle    string `db:"subtitle"`
 	ImgModifier string `db:"image_url"`
 	Author      string `db:"author"`
 	AuthorImg   string `db:"author_url"`
 	PublishDate string `db:"publish_date"`
+	PostURL     string
 }
 type recentPostData struct {
+	ID          string `db:"post_id"`
 	Title       string `db:"title"`
 	Subtitle    string `db:"subtitle"`
 	PostImage   string `db:"image_url"`
 	Author      string `db:"author"`
 	AuthorImg   string `db:"author_url"`
 	PublishDate string `db:"publish_date"`
+	PostURL     string
 }
+
+// type orderListData struct {
+// 	OrderID string `db:"order_id"`
+// 	Title   string `db:"title"`
+// }
+
+// type orderData struct {
+// 	Title   string `db:"title"`
+// 	Content string `db:"content"`
+// }
 
 // Главная страница блога
 func index(db *sqlx.DB) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
-		featuredPosts, err := getFeaturedPosts(db)
+		featured_posts, err := getFeaturedPosts(db)
 		if err != nil {
 			http.Error(w, "Internal Server Error", 500) // В случае ошибки парсинга - возвращаем 500
 			log.Println(err)
 			return // Не забываем завершить выполнение ф-ии
 		}
 
-		recentlyPosts, err := getMostRecentPosts(db)
+		recently_posts, err := getMostRecentPosts(db)
 		if err != nil {
 			http.Error(w, "Internal Server Error", 500) // В случае ошибки парсинга - возвращаем 500
 			log.Println(err)
@@ -62,8 +81,8 @@ func index(db *sqlx.DB) func(w http.ResponseWriter, r *http.Request) {
 
 		data := indexPage{
 			Title:         "Escape",
-			FeaturedPosts: featuredPosts,
-			MostRecent:    recentlyPosts,
+			FeaturedPosts: featured_posts,
+			MostRecent:    recently_posts,
 		}
 
 		err = ts.Execute(w, data) // Заставляем шаблонизатор вывести шаблон в тело ответа
@@ -77,30 +96,55 @@ func index(db *sqlx.DB) func(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func post(w http.ResponseWriter, r *http.Request) {
-	ts, err := template.ParseFiles("pages/post.html")
-	if err != nil {
-		http.Error(w, "Internal Server Error", 500)
-		log.Println(err.Error())
-		return
-	}
+func post(db *sqlx.DB) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		postIDStr := mux.Vars(r)["postID"] // Получаем postID в виде строки из параметров урла
 
-	data := postPage{
-		Title:    "The Road Ahead",
-		Subtitle: "The road ahead might be paved - it might not be.",
-	}
+		postID, err := strconv.Atoi(postIDStr) // Конвертируем строку postID в число
+		if err != nil {
+			http.Error(w, "Invalid post id", 403)
+			log.Println(err)
+			return
+		}
 
-	err = ts.Execute(w, data)
-	if err != nil {
-		http.Error(w, "Internal Server Error", 500)
-		log.Println(err.Error())
-		return
+		post, err := getPostByID(db, postID)
+		if err != nil {
+			if err == sql.ErrNoRows {
+				// sql.ErrNoRows возвращается, когда в запросе к базе не было ничего найдено
+				// В таком случае мы возвращем 404 (not found) и пишем в тело, что пост не найден
+				http.Error(w, "Post not found", 404)
+				log.Println(err)
+				return
+			}
+
+			http.Error(w, "Internal Server Error1", 500)
+			log.Println(err)
+			return
+		}
+
+		ts, err := template.ParseFiles("pages/post.html")
+		if err != nil {
+			http.Error(w, "Internal Server Error2", 500)
+			log.Println(err.Error())
+			return
+		}
+
+		err = ts.Execute(w, post)
+		if err != nil {
+			http.Error(w, "Internal Server Error", 500)
+			log.Println(err)
+			return
+		}
+
+		log.Println("Request completed successfully")
 	}
 }
 
+// Страница просмотра постов
 func getFeaturedPosts(db *sqlx.DB) ([]featuredPostData, error) {
 	const query = `
 		SELECT
+			post_id,
 			title,
 			subtitle,
 			image_url,
@@ -110,7 +154,6 @@ func getFeaturedPosts(db *sqlx.DB) ([]featuredPostData, error) {
 		FROM
 			post
 		WHERE featured = 1
-		LIMIT 2
 	`
 
 	var posts []featuredPostData
@@ -126,6 +169,7 @@ func getFeaturedPosts(db *sqlx.DB) ([]featuredPostData, error) {
 func getMostRecentPosts(db *sqlx.DB) ([]recentPostData, error) {
 	const query = `
 		SELECT
+			post_id,
 			title,
 			subtitle,
 			image_url,
@@ -135,8 +179,6 @@ func getMostRecentPosts(db *sqlx.DB) ([]recentPostData, error) {
 		FROM
 			post
 		WHERE featured = 0
-		ORDER BY post_id DESC
-		LIMIT 6
 	`
 
 	var posts []recentPostData
@@ -147,4 +189,28 @@ func getMostRecentPosts(db *sqlx.DB) ([]recentPostData, error) {
 	}
 
 	return posts, nil
+}
+
+func getPostByID(db *sqlx.DB, postID int) (postData, error) {
+	const query = `
+		SELECT
+			title,
+			subtitle,
+			image_url,
+			content
+		FROM
+			post
+		WHERE
+			post_id = ?
+	`
+
+	var postContent postData
+
+	// Обязательно нужно передать в параметрах postID
+	err := db.Get(&postContent, query, postID)
+	if err != nil {
+		return postData{}, err
+	}
+
+	return postContent, nil
 }
--- lw7/cmd/blog/main.go
+++ lw7/cmd/blog/main.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 
 	_ "github.com/go-sql-driver/mysql" // Импортируем для возможности подключения к MySQL
+	"github.com/gorilla/mux"
 	"github.com/jmoiron/sqlx"
 )
 
@@ -23,12 +24,15 @@ func main() {
 
 	dbx := sqlx.NewDb(db, dbDriverName) // Расширяем стандартный клиент к базе
 
-	mux := http.NewServeMux()
+	// Обязательно подключить github.com/gorilla/mux в импортах
+	mux := mux.NewRouter()
 	mux.HandleFunc("/home", index(dbx)) // Передаём клиент к базе данных в ф-ию обработчик запроса
-	mux.HandleFunc("/post", post)
 
-	// Реализуем отдачу статики
-	mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
+	// Указывем postID поста в URL для перехода на конкретный пост
+	mux.HandleFunc("/post/{postID}", post(dbx))
+
+	// Правим отдачу статического контента, в виду перезда на новый роутер
+	mux.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
 
 	fmt.Println("Start server")
 	err = http.ListenAndServe(port, mux)
@@ -39,5 +43,5 @@ func main() {
 
 func openDB() (*sql.DB, error) {
 	// Здесь прописываем соединение к базе данных
-	return sql.Open(dbDriverName, "root:password@tcp(localhost:3306)/blog_old?charset=utf8mb4&collation=utf8mb4_unicode_ci&parseTime=true")
+	return sql.Open(dbDriverName, "root:password@tcp(localhost:3306)/blog?charset=utf8mb4&collation=utf8mb4_unicode_ci&parseTime=true")
 }
